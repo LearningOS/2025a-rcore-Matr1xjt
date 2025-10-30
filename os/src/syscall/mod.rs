ee10231b186e37ba103d148cf01a77fbc53035e6
matr1xjt <2543182247@qq.com>
2025-10-30 10:10:02 +0800

    ch3

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -26,15 +26,44 @@ mod process;
 
 use fs::*;
 use process::*;
+/// 记录写操作的计数
+pub static mut WRITE_COUNT :usize = 0;
+/// 记录退出操作的计数
+pub static mut EXIT_COUNT :usize = 0;
+/// 记录让出操作的计数
+pub static mut YIELD_COUNT :usize = 0;
+/// 记录获取时间操作的计数
+pub static mut GET_TIME_COUNT :usize = 0; 
+/// 记录跟踪操作的计数
+pub static mut TRACE_COUNT :usize = 0;
 
 /// handle syscall exception with `syscall_id` and other arguments
 pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
     match syscall_id {
-        SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2]),
-        SYSCALL_EXIT => sys_exit(args[0] as i32),
-        SYSCALL_YIELD => sys_yield(),
-        SYSCALL_GET_TIME => sys_get_time(args[0] as *mut TimeVal, args[1]),
-        SYSCALL_TRACE => sys_trace(args[0], args[1], args[2]),
+        SYSCALL_WRITE => {
+            let res = sys_write(args[0], args[1] as *const u8, args[2]);
+            unsafe{WRITE_COUNT += 1;}
+            res
+        }
+        SYSCALL_EXIT => {
+            unsafe{EXIT_COUNT += 1;}
+            sys_exit(args[0] as i32);
+        }
+        SYSCALL_YIELD => {
+            let res = sys_yield();
+            unsafe{YIELD_COUNT += 1;}
+            res
+        }
+        SYSCALL_GET_TIME => {
+            let res = sys_get_time(args[0] as *mut TimeVal, args[1]);
+            unsafe{GET_TIME_COUNT += 1;}
+            res
+        }
+        SYSCALL_TRACE => {
+            let res = sys_trace(args[0], args[1], args[2]);
+            unsafe{TRACE_COUNT += 1;}
+            res
+        }
         _ => panic!("Unsupported syscall_id: {}", syscall_id),
     }
 }
--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -37,9 +37,41 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     }
     0
 }
-
+use crate::syscall::WRITE_COUNT;
+use crate::syscall::EXIT_COUNT;
+use crate::syscall::YIELD_COUNT;
+use crate::syscall::GET_TIME_COUNT;
+use crate::syscall::TRACE_COUNT;
+use crate::syscall::SYSCALL_WRITE;
+use crate::syscall::SYSCALL_EXIT;
+use crate::syscall::SYSCALL_YIELD;
+use crate::syscall::SYSCALL_GET_TIME;
+use crate::syscall::SYSCALL_TRACE;
 // TODO: implement the syscall
 pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
-    trace!("kernel: sys_trace");
-    -1
+    match _trace_request {
+        0 => {
+            let res = unsafe{ *(_id as *const u8)};
+            res as isize
+        }
+        1 => {
+            unsafe {
+                *(_id as *mut u8) = _data as u8;
+            }
+            0
+        }
+        2 => {
+            match _id {
+                SYSCALL_WRITE => unsafe { WRITE_COUNT as isize },
+                SYSCALL_EXIT => unsafe { EXIT_COUNT as isize },
+                SYSCALL_YIELD => unsafe { YIELD_COUNT as isize },
+                SYSCALL_GET_TIME => unsafe { GET_TIME_COUNT as isize },
+                SYSCALL_TRACE => unsafe { TRACE_COUNT as isize },
+                _ => panic!("Unsupported syscall_id: {}", _id),
+            }
+        }
+        _ => {
+            -1
+        }
+    }   
 }
